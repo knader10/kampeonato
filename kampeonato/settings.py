fefd75c110fcd9818a66d9bb0dd8a3333eb40f1a
knader10 <knader10@gmail.com>
2025-07-29 17:20:08 -0300

    Mensagem descritiva da atualização

--- kampeonato/settings.py
+++ kampeonato/settings.py
@@ -110,15 +110,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kampeonato.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
