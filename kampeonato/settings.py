02863c0cf0e0219c8344e7116bd8d52704484dab
knader10 <knader10@gmail.com>
2025-07-29 17:12:17 -0300

    Mensagem descritiva da atualização

--- kampeonato/settings.py
+++ kampeonato/settings.py
@@ -31,6 +31,22 @@ MERCADOPAGO_ACCESS_TOKEN = config('MERCADOPAGO_ACCESS_TOKEN')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
+from decouple import config
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
+
+
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
