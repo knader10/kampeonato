614d0072be7fed66c9e8f763cae87309cd87d099
knader10 <knader10@gmail.com>
2025-07-29 22:38:18 -0300

    Mensagem descritiva da atualização

--- kampeonato/settings.py
+++ kampeonato/settings.py
@@ -31,17 +31,17 @@ MERCADOPAGO_ACCESS_TOKEN = config('MERCADOPAGO_ACCESS_TOKEN')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'NAME': 'kampeonato_db',
+        'USER': 'knader10',
+        'PASSWORD': 'Khaled10!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 
